bbeac05bbc5f5e8f942e1a54c92c420cc68a9bc2
Gianpiero <gpi91mx@gmail.com>
2020-06-08 17:27:45 +0200

    Miglioramento generale

--- main.js
+++ main.js
@@ -1,32 +1,33 @@
 // SEMPLIFICO VARIABILI
-var barraMenu = $('i.fas.fa-bars');
-var hamuberMenu = $('.hamburger-menu');
-var hamburMenuClose =  $('.hamburger-menu a.close');
 var hamburMenuOpen = $('.header-right > a');
-// MOSTRO MENU AL CLICK
-barraMenu.click(
-  function() {
-    hamuberMenu.show(500);
-  }
-);
-// NASCONDO MENU AL CLICK
-hamuberMenu.click(
-  function() {
-   hamuberMenu.hide(500);
-  }
-);
+var hamburMenuClose =  $('a[href].close');
 
-
-// SECONDO METODO CON CLASSE PRE-ESISTENTE
 // MOSTRO MENU AL CLICK
 hamburMenuOpen.click(
   function() {
-    hamuberMenu.addClass('active');
+    $('.hamburger-menu').show(500);
   }
 );
+
 // NASCONDO MENU AL CLICK
-hamburMenuClose.click(
+$('.hamburger-menu').click(
   function() {
-    hamuberMenu.removeClass('active');
+   $('.hamburger-menu').hide(500);
   }
 );
+
+
+// // SECONDO METODO CON CLASSE PRE-ESISTENTE
+// // MOSTRO MENU AL CLICK
+// hamburMenuOpen.click(
+//   function() {
+//     $('.hamburger-menu').addClass('active');
+//   }
+// );
+
+// // NASCONDO MENU AL CLICK
+// hamburMenuClose.click(
+//   function() {
+//     $('.hamburger-menu').removeClass('active');
+//   }
+// );
